8262f698801785a9a8b6296705a2420f7b1f9dea
Aklilu Mengesha <aklilumengesha57@gmail.com>
2026-03-28 20:54:01 +0300

    feat: add AuthModule, UserModule, VideoModule with controllers and services

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { VideoModule } from './video/video.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, VideoModule],
   controllers: [AppController],
   providers: [AppService],
 })
